e230768672ec6dcd78ae392cce8988a8d1d97c4d
Andreas <andreas.s.bakke@gmail.com>
2021-11-10 09:42:55 +0100

    Bedre databus

--- project/verilog/pixelTop.v
+++ project/verilog/pixelTop.v
@@ -5,16 +5,19 @@ module PIXEL_TOP (
     input   logic   reset,
     input   logic   VBN,
     input   logic   RAMP,
-    inout   [7:0]   DATA1,
-    inout   [7:0]   DATA2,
-    inout   [7:0]   DATA3,
-    inout   [7:0]   DATA4
+    output   [7:0]   DATA1,
+    output   [7:0]   DATA2,
+    output   [7:0]   DATA3,
+    output   [7:0]   DATA4
 );
 
     wire erase, read, expose, convert;//Kan den være wire, eller bør den bli logic? 
 
     PIXEL_STATE pState1(clk, reset, erase, read, expose, convert);
-    PIXEL_ARRAY pa1(VBN, RAMP, reset, erase, expose, read, DATA1, DATA2, DATA3, DATA4);
+    PIXEL_ARRAY pa1(VBN, RAMP, reset, erase, expose, read, convert, DATA1, DATA2, DATA3, DATA4);
+
+    assign RAMP = convert ? clk : 0; //Så lenge denne kjører, øker adc i pixelsensoren frem til adc>tmp
+    assign VBN = expose ? clk : 0; //Clk eller 0 avhengig av om state=expose
 
 endmodule
 
